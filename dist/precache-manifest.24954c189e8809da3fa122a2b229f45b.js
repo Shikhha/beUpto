8e335abe73f05d495acd4e5b2688a029e8ec5c61
shikha jhamb <shikha.jhamb1992@gmail.com>
2019-07-03 19:23:44 +0530

    added project structure

---
+++ dist/precache-manifest.24954c189e8809da3fa122a2b229f45b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07ee145cbaaf91892afbf93e8251eaca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f2ed8dacc9e158b7e8a",
+    "url": "/static/css/main.e44744c1.chunk.css"
+  },
+  {
+    "revision": "15b0d1b079e22c66b714",
+    "url": "/static/js/2.7e7d1f0e.chunk.js"
+  },
+  {
+    "revision": "7f2ed8dacc9e158b7e8a",
+    "url": "/static/js/main.c5287761.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f683d58a944cc3f80662eb54142516b1",
+    "url": "/static/media/annie-spratt--SG84EOcaJE-unsplash.f683d58a.jpg"
+  }
+]);
