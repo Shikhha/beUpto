899cb08fcbbd3c87a22721afba64c25d601dff54
shikha jhamb <shikha.jhamb1992@gmail.com>
2019-07-05 04:45:05 +0530

    edited firebase functions

--- functions/index.js
+++ functions/index.js
@@ -1,41 +1,8 @@
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
-admin.initializeApp(functions.config().firebase);
+const functions = require('firebase-functions');
 
-const createNotification = notification => {
-  return admin
-    .firestore()
-    .collection("notification")
-    .add(notification)
-    .then(doc => {});
-};
-
-exports.projectCreated = functions.firestore
-  .document("projects/{projectId}")
-  .onCreate(doc => {
-    const project = doc.data();
-    const notification = {
-      content: "Added a new Task",
-      user: `${project.authorFirstName} ${project.authorLastName}`,
-      time: admin.firestore.FieldValue.serverTimestamp()
-    };
-
-    return createNotification(notification);
-  });
-
-exports.userJoined = functions.auth.user().onCreate(user => {
-  return admin
-    .firestore()
-    .collection("users")
-    .doc(user.uid)
-    .get()
-    .then(doc => {
-      const newUser = doc.data();
-      const notification = {
-        content: "Joined the party",
-        user: `${newUser.firstName} ${newUser.lastName}`,
-        time: admin.firestore.FieldValue.serverTimestamp()
-      };
-      return createNotification(notification);
-    });
-});
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
